140a90a0c5c96efbc6a7c73d834430902cab409c
root <root@ip-172-31-4-103.ec2.internal>
2025-01-23 10:07:18 +0000

    Jenkinsfile update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,54 +1,51 @@
-// variable "aws_access_key" {}
-// variable "aws_secret_key" {}
 
 resource "aws_security_group" "acn_sg" {
   name   = "mysecuritygroup"
-  vpc_id = "vpc-0780fe3cca77ae47b"      //change with your vpc-id
+  vpc_id = "vpc-0780fe3cca77ae47b"  # Change with your VPC ID
 
-  # SSH access from anywhere
+  # SSH access from a specific IP (recommended)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]         //by-default it open for anywhere, RECOMMENDED to restrict to your IP
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your IP address for security
   }
 
-  # HTTP access from anywhere
+  # HTTP access from anywhere (optional)
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Open to the public for HTTP
   }
 
-  # outbound internet access
-   egress {
+  # Outbound internet access
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  
-}
-
-
-#EC2-Instance
-
-resource "aws_instance" "ec2_instance"{
- ami = "ami-0df8c184d5f6ae949"        //change to your desired ami-id as it is amazon-linux support in "us-east-1" region.
- instance_type = "t2.micro"
- vpc_security_group_ids = [aws_security_group.acn_sg.id]
- key_name               = "demoserver"  // change to your key-value pair name.
- tags   = {
-   "OS" = "amazon"
- }
- root_block_device {
-    encrypted   = true
   }
 }
 
+# EC2 Instance
+resource "aws_instance" "ec2_instance" {
+  ami               = "ami-0df8c184d5f6ae949"  # Change to your desired AMI ID (Amazon Linux for us-east-1)
+  instance_type     = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.acn_sg.id]
+  key_name          = "demoserver"  # Change to your key-pair name
+  tags = {
+    "OS" = "amazon"
+  }
 
+  root_block_device {
+    encrypted = true
+  }
+}
 
+# Output the public IP of the EC2 instance
 output "public_ip" {
-  description = "The private IP address of the EC2 instance"
+  description = "The public IP address of the EC2 instance"
   value       = aws_instance.ec2_instance.public_ip
 }
+
